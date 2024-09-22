b4395ce27269121823b0b4d4c74ef8c4dfe0e82c
Mayur <147459454+Mayursoni1518@users.noreply.github.com>
2024-09-22 23:19:48 +0530

    Update Read.jsx

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -10,7 +10,7 @@ const Read = () => {
     axios.get('http://localhost:3000/users/' + id)
       .then(res => setData(res.data))
       .catch(err => console.log(err));
-    }, [])
+    }, [id])
   return (
     <div className='d-flex w-100 vh-100 justify-content-center align-items-center bg-light' >
       <div className='w-50 border bg-white shadow px-5 pt-3 pb-5 rounded'>
